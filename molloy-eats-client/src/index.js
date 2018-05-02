647c20d039ca142c28065cd44728850e70edc734
zsalvacion <salvzf92@gmail.com>
2018-05-02 12:39:42 -0400

    changed HTTP Link

--- molloy-eats-client/src/index.js
+++ molloy-eats-client/src/index.js
@@ -11,7 +11,7 @@ import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { ApolloLink } from 'apollo-link';
 
-const httpLink = new HttpLink({ uri: 'http://localhost:3000/graphql' })
+const httpLink = new HttpLink({ uri: 'https://molloy-eats.herokuapp.com/graphql' })
 const middlewareAuthLink = new ApolloLink((operation, forward) => {
   const token = localStorage.getItem('auth-token')
   const authorizationHeader = token ? `Bearer ${token}` : null
