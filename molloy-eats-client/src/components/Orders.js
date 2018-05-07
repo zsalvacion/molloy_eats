7f1d7cc382430a05964defb22653fdeb2f2a8659
zsalvacion <salvzf92@gmail.com>
2018-05-07 11:54:00 -0400

    Edited Order Page

--- molloy-eats-client/src/components/Orders.js
+++ molloy-eats-client/src/components/Orders.js
@@ -7,45 +7,35 @@ class Orders extends React.Component {
     
     constructor(props) {
         super(props);
-        this.handleUsernameChange = this.handleUsernameChange.bind(this);
-        this.handlePasswordChange = this.handlePasswordChange.bind(this);
-        this.handleLogin = this.handleLogin.bind(this);
         this.state = {
           username:'',
           password:''
         };
     }
 
-    handleUsernameChange(e){
-      this.setState({username:e.target.value});
-    };
-    handlePasswordChange(e){
-      this.setState({password:e.target.value});
-    };
-    async handleLogin(){
-      var result = await this.props.login(this.state.username, this.state.password); 
-      this.saveToken(result.data.login.token);
-      alert("Welcome " + result.data.login.user); 
-      this.props.history.push("/profile");
-    };
 
     render() {
         return (
-            <form>
-                <h2> Your Orders </h2>
-                <label htmlFor="inputUsername"> Username </label>
-                <input onChange={value => this.handleUsernameChange(value)} id="inputUsername" placeholder="Username" required autoFocus />
-                <label htmlFor="inputPassword"> Password</label>
-                <input type="password" onChange={value => this.handlePasswordChange(value)} id="inputPassword" placeholder="Password" required />
-                <button onClick={this.handleLogin} type="button"> Sign in</button>
-                <li><Link to='/register'>Need a new account?</Link></li>
-            </form>
+            <div>
+                 <table>
+            <thead>
+              <tr id="header">
+                <th>Store Name</th>
+                <th>Total</th>
+                <th>Quantity</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1 Egg Or Egg White</td>
+                <td><input type="number" name="qnty1EggOrEggWhite" onChange={(value) => this.onChangeQuantity(value)} defaultValue="0" min="0"/></td>
+                <td>$0.95</td>
+              </tr>
+            </tbody>
+          </table>
+            </div>
         )
     }
-
-  saveToken = token => {
-    localStorage.setItem('auth-token', token);
-  }
 
 }
 
